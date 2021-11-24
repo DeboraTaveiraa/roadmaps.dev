87b71dc570cf4a78342159a07cb337807fc38bbb
Débora Taveira <deborataveirapvh@gmail.com>
2021-11-24 13:11:20 -0400

    Revert "Revert "Adjusted CSS cards""
    This reverts commit a7cea40415fc2ff546516622a9012d9f6caca249.

---
+++ roadmaps.dev/src/views/FrontEnd/FrontEnd.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper-roadmap-frontend">
+    <BreadCrumbs :items="breadCrumbs" />
+  </div>
+</template>
+
+<script>
+import BreadCrumbs from "@/components/BreadCrumbs/BreadCrumbs.vue";
+import { BREADCRUMBS } from "./constants";
+
+export default {
+  components: {
+    BreadCrumbs,
+  },
+  data() {
+    return {
+      breadCrumbs: BREADCRUMBS,
+    };
+  },
+};
+</script>
+
+<style lang="scss" src="./FrontEnd.scss" />
