423e60a4609f7f84cbfc03d4694e098b1737f525
Débora Taveira <deborataveirapvh@gmail.com>
2022-06-20 21:06:52 -0400

    feature[Footer]: adjust social media icons and add links

--- roadmaps.dev/src/components/Footer/Footer.vue
+++ roadmaps.dev/src/components/Footer/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <footer class="footer">
     <div>
-      <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+      <v-btn
+        v-for="(icon, index) in social.icons"
+        :key="icon"
+        class="mx-4"
+        dark
+        icon
+        @click="goToSocial(social.link[index])"
+      >
         <v-icon size="24px" color="#212121">
           {{ icon }}
         </v-icon>
@@ -20,8 +27,20 @@
 <script>
 export default {
   data: () => ({
-    icons: ["mdi-twitter", "mdi-instagram"],
+    social: {
+      icons: ["mdi-instagram", "mdi-linkedin", "mdi-github"],
+      link: [
+        "https://www.instagram.com/deborataveiracode/",
+        "https://www.linkedin.com/in/deborataveiraa/",
+        "https://github.com/DeboraTaveiraa",
+      ],
+    },
   }),
+  methods: {
+    goToSocial(link) {
+      window.open(link, "_blank");
+    },
+  },
 };
 </script>
 
